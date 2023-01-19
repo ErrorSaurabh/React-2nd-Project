64a038413d1d20b3847b26757473c3fe8a0a3eaf
ErrorSaurabh <saurabhchauragade69@gmail.com>
2023-01-19 11:28:49 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-
+import NavbarComp from './components/NavbarComp';
+import Card from './components/Card';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NavbarComp/>
+      <Card title="Shri Ekanath Shinde" desc="Chief Minister of Maharashtra" button="Learn More" imgUrl="https://mamfdc.maharashtra.gov.in/sites/default/files/other_images/Eknath_Shinde.jpg"/>
+      <Card title="Shri Devendra Fadnavis" desc="Deputy Chief Minister of Maharashtra" button="Learn More" imgUrl="https://static.indiatvnews.com/ins-web/images/cm-pic-big-1542251665.jpg"/>
+      <Card title="Shri Sudhir Mungantiwar" desc="Ministry of Forests Department" button="Learn More" imgUrl="https://www.electwise.in/media/images/people/photo/Mungantiwar_Sudhir_Sachchidanand.jpg"/>
+      <Card title="Shri Chandrakant Patil" desc="Ministry of Higher Education and Technical Education" button="Learn More" imgUrl="https://m.economictimes.com/thumb/msid-70244881,width-1200,height-900,resizemode-4,imgsize-170123/chandrakant-patil-agencies.jpg"/>
+    </>
   );
 }
 
